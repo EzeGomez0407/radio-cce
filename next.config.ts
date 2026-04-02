ff678f79365a163aa3a148b39df756b33d11a4b5
EzeGomez0407 <gomezmerloe@gmail.com>
2026-04-01 21:01:21 -0300

    agregamos imagen en el home, acomodamos btn de agregar evento y otras cositas mas

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  
+  images: {
+    remotePatterns: [new URL('https://res.cloudinary.com/djqttard2/image/upload/q_auto/f_auto/v1775087593/Gemini_Generated_Image_mkv5zdmkv5zdmkv5_qaqy2o.png')]
+  }
 };
 
 export default nextConfig;
